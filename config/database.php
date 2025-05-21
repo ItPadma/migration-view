ff5c3445b4f5218c5bcff4b3eeed28ed9a064d92
AlvifSandana <alvif.sandana@padmatirtagroup.com>
2025-05-21 19:58:22 +0700

    Enable encryption options for database connections in configuration

--- config/database.php
+++ config/database.php
@@ -108,8 +108,8 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'true'),
         ],
 
         'sqlsrv_252' => [
@@ -123,7 +123,7 @@ return [
             'charset' => env('DB_252_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_252_ENCRYPT', 'yes'),
+            'encrypt' => env('DB_252_ENCRYPT', 'yes'),
             'trust_server_certificate' => env('DB_252_TRUST_SERVER_CERTIFICATE', 'true'),
         ],
 
